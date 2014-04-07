45cb426ca51745d158fe947097cd8f3634b108f2
hrishikeshh <hrishikeshh@vm-0.hrishikeshh.koding.kd.io>
2014-04-07 10:09:11 +0000

    lol

--- js/main.js
+++ js/main.js
@@ -2,7 +2,11 @@
 function vineetafy(text)
 {
   var input = text;
-  var to_array = ["i ", "I ", "is ", "you "];
+  if(input[input.length-1]!=".")
+  {
+    input= input +".";
+  }
+  var to_array = ["i ", "I ", "is ", "you ","are "];
   //"I am ", "i am ","you are "
   var to_array_replace = [];
   for(var i=0;i<to_array.length;i++)
